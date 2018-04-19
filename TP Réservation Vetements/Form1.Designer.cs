cc2fcb1acf09c24e12d0af2ebd075fe1acbbe9a8
Quentin <goujac.quentin@gmail.com>
2018-04-19 14:00:47 +0200

    Troisième Commit Quentin

--- TP Réservation Vetements/Form1.Designer.cs
+++ TP Réservation Vetements/Form1.Designer.cs
@@ -28,49 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Produit = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.Taille = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.Aperçu = new System.Windows.Forms.Label();
-            this.Sexe = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.vêtementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hommeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tShirtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vestesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pantalonJeansToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chaussuresBasketsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.femmeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tShirtToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.vestesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pantalonJeansToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.chaussuresBasketsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Produit
-            // 
-            this.Produit.AutoSize = true;
-            this.Produit.Location = new System.Drawing.Point(90, 37);
-            this.Produit.Name = "Produit";
-            this.Produit.Size = new System.Drawing.Size(46, 13);
-            this.Produit.TabIndex = 0;
-            this.Produit.Text = "Produit :";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(142, 48);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
-            
-            // 
-            // Taille
-            // 
-            this.Taille.AutoSize = true;
-            this.Taille.Location = new System.Drawing.Point(95, 102);
-            this.Taille.Name = "Taille";
-            this.Taille.Size = new System.Drawing.Size(41, 13);
-            this.Taille.TabIndex = 2;
-            this.Taille.Text = "Taille : ";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(142, 113);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(64, 21);
-            this.comboBox2.TabIndex = 3;
             // 
             // Aperçu
             // 
@@ -81,54 +55,146 @@
             this.Aperçu.TabIndex = 4;
             this.Aperçu.Text = "Aperçu :";
             // 
-            // Sexe
+            // menuStrip1
             // 
-            this.Sexe.AutoSize = true;
-            this.Sexe.Location = new System.Drawing.Point(95, 159);
-            this.Sexe.Name = "Sexe";
-            this.Sexe.Size = new System.Drawing.Size(37, 13);
-            this.Sexe.TabIndex = 5;
-            this.Sexe.Text = "Sexe :";
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vêtementToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(950, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // comboBox3
+            // vêtementToolStripMenuItem
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(142, 174);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(64, 21);
-            this.comboBox3.TabIndex = 6;
-            this.comboBox3.Text = "Femme";
-                
+            this.vêtementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hommeToolStripMenuItem,
+            this.femmeToolStripMenuItem});
+            this.vêtementToolStripMenuItem.Name = "vêtementToolStripMenuItem";
+            this.vêtementToolStripMenuItem.Size = new System.Drawing.Size(139, 20);
+            this.vêtementToolStripMenuItem.Text = "Passer une commande";
+            // 
+            // hommeToolStripMenuItem
+            // 
+            this.hommeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tShirtToolStripMenuItem,
+            this.vestesToolStripMenuItem,
+            this.pantalonJeansToolStripMenuItem,
+            this.chaussuresBasketsToolStripMenuItem});
+            this.hommeToolStripMenuItem.Name = "hommeToolStripMenuItem";
+            this.hommeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hommeToolStripMenuItem.Text = "Homme";
+            // 
+            // tShirtToolStripMenuItem
+            // 
+            this.tShirtToolStripMenuItem.Name = "tShirtToolStripMenuItem";
+            this.tShirtToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.tShirtToolStripMenuItem.Text = "T-Shirt";
+            this.tShirtToolStripMenuItem.Click += new System.EventHandler(this.tShirtToolStripMenuItem_Click);
+            // 
+            // vestesToolStripMenuItem
+            // 
+            this.vestesToolStripMenuItem.Name = "vestesToolStripMenuItem";
+            this.vestesToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.vestesToolStripMenuItem.Text = "Vestes";
+            this.vestesToolStripMenuItem.Click += new System.EventHandler(this.vestesToolStripMenuItem_Click);
+            // 
+            // pantalonJeansToolStripMenuItem
+            // 
+            this.pantalonJeansToolStripMenuItem.Name = "pantalonJeansToolStripMenuItem";
+            this.pantalonJeansToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.pantalonJeansToolStripMenuItem.Text = "Pantalon / Jeans";
+            this.pantalonJeansToolStripMenuItem.Click += new System.EventHandler(this.pantalonJeansToolStripMenuItem_Click);
+            // 
+            // chaussuresBasketsToolStripMenuItem
+            // 
+            this.chaussuresBasketsToolStripMenuItem.Name = "chaussuresBasketsToolStripMenuItem";
+            this.chaussuresBasketsToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.chaussuresBasketsToolStripMenuItem.Text = "Chaussures / Baskets";
+            this.chaussuresBasketsToolStripMenuItem.Click += new System.EventHandler(this.chaussuresBasketsToolStripMenuItem_Click);
+            // 
+            // femmeToolStripMenuItem
+            // 
+            this.femmeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tShirtToolStripMenuItem1,
+            this.vestesToolStripMenuItem1,
+            this.pantalonJeansToolStripMenuItem1,
+            this.chaussuresBasketsToolStripMenuItem1});
+            this.femmeToolStripMenuItem.Name = "femmeToolStripMenuItem";
+            this.femmeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.femmeToolStripMenuItem.Text = "Femme";
+            // 
+            // tShirtToolStripMenuItem1
+            // 
+            this.tShirtToolStripMenuItem1.Name = "tShirtToolStripMenuItem1";
+            this.tShirtToolStripMenuItem1.Size = new System.Drawing.Size(184, 22);
+            this.tShirtToolStripMenuItem1.Text = "T-Shirt";
+            this.tShirtToolStripMenuItem1.Click += new System.EventHandler(this.tShirtToolStripMenuItem1_Click);
+            // 
+            // vestesToolStripMenuItem1
+            // 
+            this.vestesToolStripMenuItem1.Name = "vestesToolStripMenuItem1";
+            this.vestesToolStripMenuItem1.Size = new System.Drawing.Size(184, 22);
+            this.vestesToolStripMenuItem1.Text = "Vestes";
+            this.vestesToolStripMenuItem1.Click += new System.EventHandler(this.vestesToolStripMenuItem1_Click);
+            // 
+            // pantalonJeansToolStripMenuItem1
+            // 
+            this.pantalonJeansToolStripMenuItem1.Name = "pantalonJeansToolStripMenuItem1";
+            this.pantalonJeansToolStripMenuItem1.Size = new System.Drawing.Size(184, 22);
+            this.pantalonJeansToolStripMenuItem1.Text = "Pantalon / Jeans";
+            this.pantalonJeansToolStripMenuItem1.Click += new System.EventHandler(this.pantalonJeansToolStripMenuItem1_Click);
+            // 
+            // chaussuresBasketsToolStripMenuItem1
+            // 
+            this.chaussuresBasketsToolStripMenuItem1.Name = "chaussuresBasketsToolStripMenuItem1";
+            this.chaussuresBasketsToolStripMenuItem1.Size = new System.Drawing.Size(184, 22);
+            this.chaussuresBasketsToolStripMenuItem1.Text = "Chaussures / Baskets";
+            this.chaussuresBasketsToolStripMenuItem1.Click += new System.EventHandler(this.chaussuresBasketsToolStripMenuItem1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(511, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 354);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 447);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.Sexe);
+            this.ClientSize = new System.Drawing.Size(950, 441);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Aperçu);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.Taille);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.Produit);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Produit;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label Taille;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label Aperçu;
-        private System.Windows.Forms.Label Sexe;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem vêtementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hommeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tShirtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vestesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pantalonJeansToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chaussuresBasketsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem femmeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tShirtToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem vestesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pantalonJeansToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem chaussuresBasketsToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- TP Réservation Vetements/Form1.cs
+++ TP Réservation Vetements/Form1.cs
@@ -22,5 +22,44 @@ namespace TP_Réservation_Vetements
 
         }
 
+        private void tShirtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Tshirt_Homme.jpg";
+        }
+
+        private void vestesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Veste_Homme.jpg";
+        }
+
+        private void pantalonJeansToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Pantalon_Homme.jpg";
+        }
+
+        private void chaussuresBasketsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Chaussure_Homme.jpg";
+        }
+
+        private void tShirtToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Tshirt_Femme.jpg";
+        }
+
+        private void vestesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Veste_Femme.jpg";
+        }
+
+        private void pantalonJeansToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Pantalon_Femme.jpg";
+        }
+
+        private void chaussuresBasketsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "C:/Users/Delphine/Documents/Tp_Visual_Studio/TP_Réservation_Vetements/Images/Chaussure_Femme.jpg";
+        }
     }
 }
